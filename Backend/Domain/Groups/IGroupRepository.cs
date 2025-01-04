3eda401a9c887d2805994957860428ed66d029a0
alejandroG <alejvo05@gmail.com>
2025-01-03 23:02:02 -0500

    Add Group Repository Logic

--- Backend/Domain/Groups/IGroupRepository.cs
+++ Backend/Domain/Groups/IGroupRepository.cs
@@ -4,6 +4,7 @@
     {
         Task<IEnumerable<Group>> GetAll();
         Task<Group?> GetById(string id);
+        Task<IEnumerable<Group>> GetByName(string name);
         Task CreateAsync(object param);
         Task UpdateAsync(object param);
         Task DeleteAsync(object param);
--- Backend/Infrastructure/Repositories/GroupRepository.cs
+++ Backend/Infrastructure/Repositories/GroupRepository.cs
@@ -1,37 +1,68 @@
-﻿using Domain.Groups;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Dapper;
+using Domain.Groups;
+using Infrastructure.Factories;
+using System.Data;
 
-namespace Infrastructure.Repositories
+namespace Infrastructure.Repositories;
+
+public class GroupRepository : IGroupRepository
 {
-    public class GroupRepository : IGroupRepository
+    private readonly SqlConnectionFactory _sqlConnection;
+
+    public GroupRepository(SqlConnectionFactory sqlConnection)
     {
-        public Task CreateAsync(object param)
-        {
-            throw new NotImplementedException();
-        }
+        _sqlConnection = sqlConnection;
+    }
 
-        public Task DeleteAsync(object param)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task CreateAsync(object param)
+    {
+        using var connection = _sqlConnection.CreateConnection();
+        await connection.ExecuteAsync(
+            GroupProcedures.CreateGroup,
+            param,
+            commandType: CommandType.StoredProcedure);
+    }
 
-        public Task<IEnumerable<Group>> GetAll()
-        {
-            throw new NotImplementedException();
-        }
+    public async Task DeleteAsync(object param)
+    {
+        using var connection = _sqlConnection.CreateConnection();
+        await connection.ExecuteAsync(
+            GroupProcedures.DeleteGroup,
+            param,
+            commandType: CommandType.StoredProcedure
+        );
+    }
 
-        public Task<Group?> GetById(string id)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<IEnumerable<Group>> GetAll()
+    {
+        using var connection = _sqlConnection.CreateConnection();
+        return await connection.QueryAsync<Group>(GroupProcedures.GetGroups);
+    }
 
-        public Task UpdateAsync(object param)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<Group?> GetById(string id)
+    {
+        using var connection = _sqlConnection.CreateConnection();
+        return await connection.QueryFirstOrDefaultAsync<Group>
+            (
+                GroupProcedures.GetGroupById,
+                new {id}
+            );
+    }
+
+    public async Task<IEnumerable<Group>> GetByName(string name)
+    {
+        using var connection = _sqlConnection.CreateConnection();
+        return await connection.QueryAsync<Group>(
+            GroupProcedures.GetGroupsName,
+            commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task UpdateAsync(object param)
+    {
+        using var connection = _sqlConnection.CreateConnection();
+        await connection.ExecuteAsync(
+            GroupProcedures.UpdateGroup,
+            param,
+            commandType: CommandType.StoredProcedure);
     }
 }
