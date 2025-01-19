bde4736e7022898a0d7910991d0de15b3be5335f
alejandroG <alejvo05@gmail.com>
2025-01-19 18:29:38 -0500

    Added Frontend Auth Feature

---
+++ Frontend/src/app/features/auth/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  @ViewChild('passwordInput') input!: ElementRef;
+  public email: string = '';
+  public password: string = '';
+  public isModalVisible = false;
+  constructor(private authService: AuthService, private router: Router) { }
+
+  login() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (success) => {
+        if (success) {
+          this.router.navigate(['/chat']);
+        } else {
+          alert('Login Failed');
+        }
+      }
+    })
+  }
+
+  showModal() {
+    this.isModalVisible = !this.isModalVisible;
+  }
+  setfocus() {
+    this.input.nativeElement.focus();
+  }
+}
---
+++ Frontend/src/app/features/auth/services/auth.service.ts
@@ -0,0 +1,73 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, map, Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { jwtDecode } from 'jwt-decode';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http:HttpClient,private router:Router) { }
+
+  private apiUrl = environment.apiUrl;
+
+  private decodeToken(token: string): any {
+    try {
+      return jwtDecode(token);
+    } catch (error) {
+      console.error('Token error', error);
+      return null;
+    }
+  }
+  getUserInfoFromToken(token: string): { userId: string, username: string } | null {
+    const decodedToken = this.decodeToken(token);
+    if (decodedToken) {
+      const userId = decodedToken.sub;
+      const username = decodedToken.unique_name;
+      return { userId, username }
+    }
+    return null;
+  }
+  login(email: string, password: string): Observable<boolean> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const params = { email, password }
+    return this.http.post<any>(`${this.apiUrl}/api/users/login`, params, { headers, observe: 'response' })
+      .pipe(
+        map((response) => {
+          if (response.status === 200 && response.body.token) {
+            localStorage.setItem('token', response.body.token);
+            return true;
+          }
+          return false;
+        }),
+        catchError(error => {
+          console.error('Login Failed', error);
+          return of(false);
+        })
+      )
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+    this.router.navigate(['/home'])
+  }
+
+  isLoggedIn(): boolean {
+
+    const token: string | null = this.getToken();
+    if (token == null) {
+      return false;
+    }
+    const decodedToken = this.decodeToken(token);
+    const currentTime = Math.floor(Date.now() / 1000);
+    return decodedToken.exp < currentTime;
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+}
